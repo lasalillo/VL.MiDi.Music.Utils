b70a4e85955280997958ab01a84c66d141662f98
Aristides Garcia <aristides_job@hotmail.com>
2024-02-20 13:19:04 +0100

    Added Message maker, Nuget, Enums changed

--- src/Enums/BooleanOperations.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Enums
-{
-    public enum BooleanOperations { OR, AND, AND_NOT, XOR, A, B };
-}
---
+++ src/Enums/MusicUtilEnums.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enums
+{
+    public enum BooleanOperation { OR, AND, AND_NOT, XOR, A, B };
+    public enum MessageCommand { NoteOn, PolyPressure, Controller, ChannelPressure, PitchWheel };
+}
